8f9d3632307d88ee3b841a63bd0ae86baff0899b
voov-v <yumin5482@naver.com>
2026-05-20 11:11:47 +0900

    Add : 프로젝트, 리비전 컨트롤 추가

---
+++ Source/Pentagram/Pentagram.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Pentagram.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pentagram, "Pentagram" );
+
+DEFINE_LOG_CATEGORY(LogPentagram)
+ 
