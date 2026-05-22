a2ad14ba00de105f128fcc03c7914553ae469974
SSH1202 <tmdghks9812@gmail.com>
2026-05-22 10:27:41 +0900

    [송승환] BaseCharacter 구현

--- Source/Pentagram/Character/PTBaseCharacter.cpp
+++ Source/Pentagram/Character/PTBaseCharacter.cpp
@@ -1,13 +1,57 @@
 #include "Character/PTBaseCharacter.h"
 
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
 APTBaseCharacter::APTBaseCharacter()
 {
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 }
+
+float APTBaseCharacter::ApplyDamage(float DamageAmount, AActor* Attacker)
+{
+    if (!HasAuthority()) return 0.f;
+
+    // 데미지 계산
+    float FinalDamage = FMath::Max(DamageAmount - BaseDef, 1.f);
+
+    //HP 감소
+    CurrentHP = FMath::Max(CurrentHP - FinalDamage,0.f);
+
+    // 죽음
+    if (CurrentHP <= 0.f)
+    {
+        OnDeath();
+    }
+
+    return FinalDamage;
+}
+
+void APTBaseCharacter::OnDeath()
+{
+    // 이동 불가
+    GetCharacterMovement()->DisableMovement();
+
+    // 콜리전 비활성화(액터끼리 충돌 안함, 나중에 리스폰 시에 활성화 시켜줘야 할 수 있음.)
+    GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+    //사망 애니메이션 재생은 각 파생 클래스에서 구현해주세요.
+}
+
 void APTBaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
+    // DT에서 캐릭터 스탯을 로드
+    if (const FPTCharacterRow* Row = CharacterDataHandle.GetRow<FPTCharacterRow>(TEXT("BeginPlay")))
+    {
+        MaxHP = Row->MaxHP;
+        CurrentHP = Row->MaxHP;
+        BaseDef = Row->BaseDef;
+        BaseAtk = Row->BaseAtk;
+        AttackSpeed = Row->AttackSpeed;
+        MoveSpeed = Row->MoveSpeed;
+    }
 }
 void APTBaseCharacter::Tick(float DeltaTime)
 {
