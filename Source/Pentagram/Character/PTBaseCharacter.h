4b89207964d8ab68d743ae81ed0f332dce7a49a4
SSH1202 <tmdghks9812@gmail.com>
2026-05-22 20:12:22 +0900

    [송승환] BasePlayState 추가, BaseCharacter 수정

--- Source/Pentagram/Character/PTBaseCharacter.h
+++ Source/Pentagram/Character/PTBaseCharacter.h
@@ -24,22 +24,23 @@ public:
     UPROPERTY(EditAnywhere, Category = "Data")
     FDataTableRowHandle CharacterDataHandle;
 
-    UPROPERTY(VisibleAnywhere, Category = "Stats")
+    UPROPERTY(Replicated, VisibleAnywhere, Category = "Stats")
     float CurrentHP;   // 현재 체력
-    UPROPERTY(VisibleAnywhere, Category = "Stats")
+    UPROPERTY(Replicated, VisibleAnywhere, Category = "Stats")
     float MaxHP;       // 최대 체력
-    UPROPERTY(VisibleAnywhere, Category = "Stats")
+    UPROPERTY(Replicated, VisibleAnywhere, Category = "Stats")
     float BaseDef;     // 방어력
-    UPROPERTY(VisibleAnywhere, Category = "Stats")
+    UPROPERTY(Replicated, VisibleAnywhere, Category = "Stats")
     float BaseAtk;     // 공격력
-    UPROPERTY(VisibleAnywhere, Category = "Stats")
+    UPROPERTY(Replicated, VisibleAnywhere, Category = "Stats")
     float AttackSpeed; // 공격 속도
-    UPROPERTY(VisibleAnywhere, Category = "Stats")
+    UPROPERTY(Replicated, VisibleAnywhere, Category = "Stats")
     float MoveSpeed;   // 이동 속도
 
 protected:
 	virtual void BeginPlay() override;
 
+    virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 public:
 	virtual void Tick(float DeltaTime) override;
 
