68422938105a5e7780ddc01e1eae8dba06d55164
voov-v <yumin5482@naver.com>
2026-05-28 15:06:09 +0900

    [변유민] AnimNotify_Attack 생성

---
+++ Source/Pentagram/Character/Monsters/PTAnimNotify_Attack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "PTAnimNotify_Attack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PENTAGRAM_API UPTAnimNotify_Attack : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
