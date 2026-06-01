e383522cec10f7161be115db7c2d512d13104b92
MIW-hub <miws4747@gmail.com>
2026-06-01 10:52:23 +0900

    인벤토리 클래스 추가

--- Source/Pentagram/UI/Data/PTDelegates.h
+++ Source/Pentagram/UI/Data/PTDelegates.h
@@ -1,34 +1,22 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
-// TODO: 아래에 슬롯 데이터 구조체들이 정의된 헤더 파일을 인클루드 하세요.
-// #include "PTSkillSlotData.h"
-// #include "PTItemSlotData.h"
-
 #include "PTDelegates.generated.h"
 
+USTRUCT(BlueprintType)
+struct FPTDelegateDummy
+{
+    GENERATED_BODY()
+};
 
-// 캐릭터 스탯
-
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FPTOnHealthChanged, float, Current, float, Max);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FPTOnManaChanged, float, Current, float, Max);
+// 매개변수 이름이 내부 매크로와 충돌하지 않도록 변경
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FPTOnHealthChanged, float, CurrentHP, float, MaxHP);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FPTOnManaChanged, float, CurrentMP, float, MaxMP);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam (FPTOnLevelChanged, int32, NewLevel);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FPTOnExpChanged, float, Current, float, Required);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FPTOnExpChanged, float, CurrentExp, float, RequiredExp);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam (FPTOnGoldChanged, int64, NewAmount);
-
-//스킬
-
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FPTOnSkillSlotChanged, int32, SlotIndex, FPTSkillSlotData, SlotData);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FPTOnSkillCooldownUpdated, int32, SlotIndex, float, RemainingSeconds);
-
-// 인벤토리/장비
-
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FPTOnInventoryChanged, const TArray<FPTItemSlotData>&, Slots);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FPTOnEquipmentChanged);
-
-// 전투(보스)
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam (FPTOnBossSpawned, AActor*, BossActor);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam (FPTOnBossPhaseChanged, int32, NewPhase);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FPTOnBossHealthChanged, float, Current, float, Max);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FPTOnBossHealthChanged, float, CurrentHP, float, MaxHP);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FPTOnBossDefeated);
---
+++ Source/Pentagram/UI/Screens/Inventory/PTEquipSlotWidget.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PTInventorySlotWidget.h"
+#include "PTEquipSlotWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class PENTAGRAM_API UPTEquipSlotWidget : public UPTInventorySlotWidget
+{
+    GENERATED_BODY()
+
+public:
+    /** 이 슬롯에 들어올 수 있는 타입 (Weapon / Chest). BP에서 슬롯별로 지정. */
+    UFUNCTION(BlueprintPure, Category = "PT|Equip")
+    EItemType GetAllowedType() const { return AllowedType; }
+
+    /** 해당 아이템이 이 장비 슬롯에 장착 가능한지. */
+    UFUNCTION(BlueprintPure, Category = "PT|Equip")
+    bool CanAccept(const FItemData& Item) const
+    {
+        return Item.Item_Category == EItemCategory::Equipment
+            && Item.Item_Type == AllowedType;
+    }
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PT|Equip")
+    EItemType AllowedType = EItemType::Weapon;
+};
