0633658c876b876c1591aeeb3e803c6266cda843
voov-v <yumin5482@naver.com>
2026-06-01 10:43:16 +0900

    [변유민] 클래스 생성

---
+++ Source/Pentagram/Character/Monsters/PTBossMonsterCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/Monsters/PTMonsterCharacter.h"
+#include "PTBossMonsterCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PENTAGRAM_API APTBossMonsterCharacter : public APTMonsterCharacter
+{
+	GENERATED_BODY()
+	
+};
