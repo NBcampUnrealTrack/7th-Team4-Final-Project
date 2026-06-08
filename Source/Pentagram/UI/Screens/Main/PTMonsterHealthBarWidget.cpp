9533adb9e0363fbd642ead18fd18db1edfe3eee2
MIW-hub <miws4747@gmail.com>
2026-06-08 17:03:34 +0900

    몬스터 hp UI클라스 추가

---
+++ Source/Pentagram/UI/Screens/Main/PTMonsterHealthBarWidget.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PTMonsterHealthBarWidget.h"
+#include "Components/ProgressBar.h"
+#include "Character/Player/PTBasePlayerState.h"
+
+void UPTMonsterHealthBarWidget::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+    if (PB_Bar)
+    {
+        PB_Bar->SetBarFillType(EProgressBarFillType::LeftToRight);
+    }
+}
+
+void UPTMonsterHealthBarWidget::HandleHealthChanged(float Current, float Max)
+{
+    UE_LOG(LogTemp, Warning, TEXT("HandleHealthChanged %f / %f"), Current, Max);
+    SetValue(Current, Max);
+}
+
+void UPTMonsterHealthBarWidget::BindToPlayerState(APTBasePlayerState* PS)
+{
+    PS->OnHealthChanged.AddUniqueDynamic(this, &UPTMonsterHealthBarWidget::HandleHealthChanged);
+}
+void UPTMonsterHealthBarWidget::UnbindFromPlayerState(APTBasePlayerState* PS)
+{
+    PS->OnHealthChanged.RemoveDynamic(this, &UPTMonsterHealthBarWidget::HandleHealthChanged);
+}
---
+++ Source/Pentagram/UI/Screens/Main/PTMonsterHealthBarWidget.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PTStatBarWidget.h"
+#include "UI/Data/PTDelegates.h"
+#include "PTMonsterHealthBarWidget.generated.h"
+
+/**
+ *
+ */
+class APTMonsterCharacter;
+UCLASS()
+class PENTAGRAM_API UPTMonsterHealthBarWidget : public UPTStatBarWidget
+{
+    GENERATED_BODY()
+
+public:
+
+    UFUNCTION()
+    void HandleHealthChanged(float Current, float Max);
+
+protected:
+    virtual void NativeConstruct() override;
+
+    virtual void BindToPlayerState(APTBasePlayerState* PS) override;
+    virtual void UnbindFromPlayerState(APTBasePlayerState* PS) override;
+};
