3f5d71776ab59792e5b5ca69082838a3bf757471
MIW-hub <miws4747@gmail.com>
2026-06-11 14:30:19 +0900

    인트로레벨및 로고 비디오 추가

---
+++ Source/Pentagram/UI/Widget/Intro/PTIntroWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "PTIntroWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PENTAGRAM_API UPTIntroWidget : public UCommonActivatableWidget
+{
+    GENERATED_BODY()
+};
