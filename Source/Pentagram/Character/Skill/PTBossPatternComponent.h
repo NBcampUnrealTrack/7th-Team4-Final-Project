215da74c747eb104f8cf1e227b6a86a82ee51383
voov-v <yumin5482@naver.com>
2026-06-17 18:51:35 +0900

    [변유민] EBossSkillType enum 도입 및 FPTBossSkillRow 스킬 필드 확장

--- Source/Pentagram/Character/Skill/PTBossPatternComponent.h
+++ Source/Pentagram/Character/Skill/PTBossPatternComponent.h
@@ -7,6 +7,7 @@
 
 class UDataTable;
 class UPTMonsterSkillComponent;
+class APTBossProjectile;
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class PENTAGRAM_API UPTBossPatternComponent : public UActorComponent
@@ -20,16 +21,34 @@ public:
 	void PreloadAllSkills();
 
 	UFUNCTION(BlueprintCallable, Category = "PT|Boss|Pattern")
-	void ExecuteSkillForPhase(int32 Phase);
+	bool ExecuteSkillForPhase(int32 Phase);
+
+    UFUNCTION(BlueprintCallable, Category = "PT|Boss|Pattern")
+    void ExecutePendingSkill(int32 Phase);
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PT|Boss|Pattern")
 	TObjectPtr<UDataTable> BossSkillDataTable;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PT|Boss|Pattern")
+	TArray<FName> Phase0SkillRowNames;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PT|Boss|Pattern")
 	TArray<FName> Phase1SkillRowNames;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "PT|Boss|Pattern")
 	TArray<FName> Phase2SkillRowNames;
+
+    UPROPERTY(EditDefaultsOnly, Category = "PT|Boss|Pattern")
+    TSubclassOf<APTBossProjectile> ProjectileClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "PT|Boss|Pattern")
+    FName ProjectileSpawnSocket = NAME_None;
+
+    UPROPERTY(EditDefaultsOnly, Category = "PT|Boss|Pattern")
+    float ProjectileSpawnForwardOffset = 100.f;
+
+    UPROPERTY(EditDefaultsOnly, Category = "PT|Boss|Pattern")
+    float ProjectileSpawnHeightOffset = 100.f;
 
 	UPROPERTY()
 	TObjectPtr<UPTMonsterSkillComponent> SkillComponent;
@@ -38,11 +57,19 @@ protected:
 	virtual void BeginPlay() override;
     virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
+    void SpawnProjectile(const FPTBossSkillRow& RowSnapshot);
+    void SpawnAreaAttack(const FPTBossSkillRow& RowSnapshot);
+
 private:
     TPair<FName, FPTBossSkillRow*> PickNextSkill(int32 Phase);
+    AActor* GetTargetActor() const;
 
     TMap<FName, FTimerHandle> PatternCooldownTimers;
     TMap<FName, bool>         PatternCooldownFlags;
 
+    FPTBossSkillRow PendingSkillSnapshot;
+    bool bHasPendingSkill = false;
+
+    FTimerHandle AreaAttackTimer;
     bool bSkillAssetsLoaded = false;
 };
