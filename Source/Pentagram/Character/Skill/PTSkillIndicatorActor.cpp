87a6373bf152b5189dffd2f2d6488544944dcd66
SSH1202 <tmdghks9812@gmail.com>
2026-07-10 20:27:51 +0900

    [송승환]
    - moba식 스킬 지정

---
+++ Source/Pentagram/Character/Skill/PTSkillIndicatorActor.cpp
@@ -0,0 +1,74 @@
+﻿#include "PTSkillIndicatorActor.h"
+#include "Components/DecalComponent.h"
+
+APTSkillIndicatorActor::APTSkillIndicatorActor()
+{
+    PrimaryActorTick.bCanEverTick = false;
+    bReplicates = false;
+
+    Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+    SetRootComponent(Root);
+
+    Decal = CreateDefaultSubobject<UDecalComponent>(TEXT("Decal"));
+    Decal->SetupAttachment(Root);
+    // 데칼은 로컬 -X축으로 투영 → 지면에 쏘려면 액터를 아래로 눕힘
+    Decal->SetRelativeRotation(FRotator(-90.f, 0.f, 0.f));
+    Decal->SetVisibility(false);
+}
+
+void APTSkillIndicatorActor::ShowLine(const FVector& Origin, const FVector& Dir, float Range, float Width)
+{
+    if (LineMID) Decal->SetDecalMaterial(LineMID);
+    const FRotator Yaw = Dir.Rotation();
+    SetActorLocation(Origin + Dir * (Range * 0.5f));
+    SetActorRotation(FRotator(0.f, Yaw.Yaw, 0.f));
+    // DecalSize = ( Y=폭 반값, Z=길이 반값). 데칼 축 방향은 에디터에서 미세조정
+    Decal->DecalSize = FVector(256.f, Width * 0.5f, Range * 0.5f);
+    Decal->SetVisibility(true);
+}
+
+void APTSkillIndicatorActor::ShowCircle(const FVector& Center, float Radius)
+{
+    if (CircleMID) Decal->SetDecalMaterial(CircleMID);
+    SetActorLocation(Center);
+    SetActorRotation(FRotator::ZeroRotator);
+    Decal->DecalSize = FVector(256.f, Radius, Radius);
+    Decal->SetVisibility(true);
+}
+
+void APTSkillIndicatorActor::ShowCone(const FVector& Origin, const FVector& Dir, float Range, float AngleDeg)
+{
+    if (ConeMID)
+    {
+        Decal->SetDecalMaterial(ConeMID);
+        ConeMID->SetScalarParameterValue(TEXT("HalfAngle"),
+            FMath::DegreesToRadians(AngleDeg * 0.5f));
+    }
+    const FRotator Yaw = Dir.Rotation();
+    SetActorLocation(Origin);
+    SetActorRotation(FRotator(0.f, Yaw.Yaw, 0.f));
+    Decal->DecalSize = FVector(256.f, Range, Range);
+
+    // 각도는 머티리얼 파라미터로 전달
+    Decal->SetVisibility(true);
+}
+
+void APTSkillIndicatorActor::ShowSelfCircle(const FVector& Center, float Radius)
+{
+    ShowCircle(Center, Radius);
+}
+
+void APTSkillIndicatorActor::HideIndicator()
+{
+    Decal->SetVisibility(false);
+}
+
+void APTSkillIndicatorActor::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (LineMaterial)   LineMID   = UMaterialInstanceDynamic::Create(LineMaterial,   this);
+    if (CircleMaterial) CircleMID = UMaterialInstanceDynamic::Create(CircleMaterial, this);
+    if (ConeMaterial)   ConeMID   = UMaterialInstanceDynamic::Create(ConeMaterial,   this);
+}
+
---
+++ Source/Pentagram/Character/Skill/PTSkillIndicatorActor.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PTSkillIndicatorActor.generated.h"
+
+UCLASS()
+class PENTAGRAM_API APTSkillIndicatorActor : public AActor
+{
+    GENERATED_BODY()
+public:
+    APTSkillIndicatorActor();
+
+    void ShowLine(const FVector& Origin, const FVector& Dir, float Range, float Width);
+
+    void ShowCircle(const FVector& Center, float Radius);
+
+    void ShowCone(const FVector& Origin, const FVector& Dir, float Range, float AngleDeg);
+
+    void ShowSelfCircle(const FVector& Center, float Radius);
+
+    void HideIndicator();
+
+    virtual void BeginPlay() override;
+
+protected:
+    UPROPERTY(VisibleAnywhere) TObjectPtr<class USceneComponent>  Root;
+    UPROPERTY(VisibleAnywhere) TObjectPtr<class UDecalComponent>  Decal;
+
+    // 모양별 머티리얼 (에디터에서 지정)
+    UPROPERTY(EditAnywhere, Category="Indicator") TObjectPtr<UMaterialInterface> LineMaterial; // 라인
+    UPROPERTY(EditAnywhere, Category="Indicator") TObjectPtr<UMaterialInterface> CircleMaterial; // 원
+    UPROPERTY(EditAnywhere, Category="Indicator") TObjectPtr<UMaterialInterface> ConeMaterial; // 부채꼴
+
+    UPROPERTY() TObjectPtr<UMaterialInstanceDynamic> LineMID;
+    UPROPERTY() TObjectPtr<UMaterialInstanceDynamic> CircleMID;
+    UPROPERTY() TObjectPtr<UMaterialInstanceDynamic> ConeMID;
+};
