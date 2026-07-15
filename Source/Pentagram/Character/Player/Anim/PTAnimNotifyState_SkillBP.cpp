597ae06807f407aa4dfa64caa7406e02510ca779
SSH1202 <tmdghks9812@gmail.com>
2026-07-15 21:03:33 +0900

    [송승환]
    -메테오,실드 스킬 추가
    - 무기 아이템 종류 추가

---
+++ Source/Pentagram/Character/Player/Anim/PTAnimNotifyState_SkillBP.cpp
@@ -0,0 +1,50 @@
+﻿#include "PTAnimNotifyState_SkillBP.h"
+
+#include "Character/Player/PTPlayerCharacter.h"
+#include "Character/Skill/PTPlayerSkillComponent.h"
+
+void UPTAnimNotifyState_SkillBP::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+    float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+    Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+
+    APTPlayerCharacter* Owner = Cast<APTPlayerCharacter>(MeshComp->GetOwner());
+    if (!Owner || !SpawnBPClass) return;
+
+    UWorld* World = Owner->GetWorld();
+    if (!World) return;
+
+    FVector SpawnLoc;
+    FRotator SpawnRot = Owner->GetActorRotation();
+
+    if (bUseTargetLocation && Owner->SkillComp)
+    {
+        SpawnLoc = Owner->SkillComp->TargetLocation;
+    }
+    else
+    {
+        FVector Offset = Owner->GetActorForwardVector() * SpawnOffset.X
+                       + Owner->GetActorRightVector()   * SpawnOffset.Y
+                       + FVector(0.f, 0.f, SpawnOffset.Z);
+        SpawnLoc = Owner->GetActorLocation() + Offset;
+    }
+
+    FActorSpawnParameters Params;
+    Params.Owner = Owner;
+    Params.Instigator = Owner->GetInstigator();
+    Params.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+    SpawnedActor = World->SpawnActor<AActor>(SpawnBPClass, SpawnLoc, SpawnRot, Params);
+}
+
+void UPTAnimNotifyState_SkillBP::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+    const FAnimNotifyEventReference& EventReference)
+{
+    Super::NotifyEnd(MeshComp, Animation, EventReference);
+
+    if (SpawnedActor.IsValid())
+    {
+        SpawnedActor->Destroy();
+        SpawnedActor = nullptr;
+    }
+}
---
+++ Source/Pentagram/Character/Player/Anim/PTAnimNotify_SkillCastVFX.cpp
@@ -0,0 +1,28 @@
+﻿#include "PTAnimNotify_SkillCastVFX.h"
+
+#include "NiagaraFunctionLibrary.h"
+#include "Character/Player/PTPlayerCharacter.h"
+#include "Character/Skill/PTSkillRow.h"
+#include "Character/Skill/PTPlayerSkillComponent.h"
+
+void UPTAnimNotify_SkillCastVFX::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                        const FAnimNotifyEventReference& EventReference)
+{
+    Super::Notify(MeshComp, Animation, EventReference);
+
+    APTPlayerCharacter* Owner = Cast<APTPlayerCharacter>(MeshComp->GetOwner());
+    if (!Owner) return;
+
+    UPTPlayerSkillComponent* SkillComp = Owner->SkillComp;
+    if (!SkillComp) return;
+
+    FPTSkillRow* SkillData = SkillComp->GetSkillData(SkillComp->GetCurrentSkillID());
+    if (!SkillData) return;
+
+    FVector Center = SkillComp->TargetLocation;
+
+    UNiagaraSystem* CastVFX = CastVFXOverride.LoadSynchronous();
+    if (CastVFX)
+        UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+            Owner->GetWorld(), CastVFX, Center, FRotator::ZeroRotator);
+}
