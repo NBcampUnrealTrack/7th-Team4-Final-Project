0ca6d0b8be7793a695bdb50a5629579c4529e961
voov-v <yumin5482@naver.com>
2026-07-15 18:31:58 +0900

    [변유민] 보스 실드 페이즈 구현 (HP 50% 진입 시 돔 전개·밀어내기·시간초과 회복)

--- Source/Pentagram/Character/Monsters/Skill/PTBossShield.h
+++ Source/Pentagram/Character/Monsters/Skill/PTBossShield.h
@@ -1,17 +1,65 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Character/PTBaseCharacter.h"
 #include "PTBossShield.generated.h"
 
-/**
- * 
- */
+class APTBossMonsterCharacter;
+class UNiagaraSystem;
+class UStaticMeshComponent;
+class UMaterialInterface;
+class UMaterialInstanceDynamic;
+class USoundBase;
+
 UCLASS()
 class PENTAGRAM_API APTBossShield : public APTBaseCharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+    APTBossShield();
+
+    void InitShield(APTBossMonsterCharacter* InOwningBoss, float InMaxHP, float InRadius, float InPushForce);
+
+    virtual float ApplyDamageWithHit(float DamageAmount, AActor* Attacker, const FPTHitInfo& HitInfo) override;
+
+    float GetCurrentShieldHP() const { return CurrentHP; }
+
+    void SuppressDestroyNotify() { bSuppressDestroyNotify = true; }
+
+protected:
+    virtual void ApplyHit(const FPTHitInfo& HitInfo) override;
+    virtual void OnDeath() override;
+    virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+    UPROPERTY(EditDefaultsOnly, Category = "PT|Boss|Shield")
+    TSoftObjectPtr<UMaterialInterface> ShieldMaterial;
+
+    UPROPERTY(EditDefaultsOnly, Category = "PT|Boss|Shield")
+    TSoftObjectPtr<UNiagaraSystem> HitImpactEffect;
+
+    UPROPERTY(EditDefaultsOnly, Category = "PT|Boss|Shield")
+    TSoftObjectPtr<UNiagaraSystem> BreakEffect;
+
+    UPROPERTY(EditDefaultsOnly, Category = "PT|Sound")
+    TSoftObjectPtr<USoundBase> HitSound;
+
+    UPROPERTY(EditDefaultsOnly, Category = "PT|Sound")
+    TSoftObjectPtr<USoundBase> BreakSound;
+
+private:
+    void PushActorOut(AActor* TargetActor);
+
+    UPROPERTY(VisibleAnywhere, Category = "PT|Boss|Shield")
+    TObjectPtr<UStaticMeshComponent> ShieldMeshComp;
+
+    UPROPERTY()
+    TObjectPtr<UMaterialInstanceDynamic> ShieldMID;
+
+    TWeakObjectPtr<APTBossMonsterCharacter> OwningBoss;
+
+    float ShieldRadius = 500.f;
+    float PushForce = 1500.f;
+
+    bool bSuppressDestroyNotify = false;
 };
